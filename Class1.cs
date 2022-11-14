dd79b9c4694f06b86ba208eab91b2de0d13a129f
Moataz Gamal <moatazgamal199@gmail.com>
2022-11-14 14:37:27 +0200

    My First Commit

---
+++ Class1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snakes_and_Ladders
+{
+    public static class Switch
+    {
+        public static bool flag = true; // flag for player 1 
+        public static bool flag7 = true; // flag for player 2
+        public static int counter1 = 1;
+        public static int counter2 = 1;
+
+
+
+        public static Point SwitchPoint1(int counter, Point p)
+        {
+
+            // Ladder Switch for Player 1
+            switch (counter)
+            {
+                case 2:
+                    counter1 = 38;
+                    flag = false;
+                    return new Point(298, 254);
+
+
+
+
+                case 7:
+                    counter1 = 14;
+                    flag = false;
+                    return new Point(439, 323);
+
+
+                case 8:
+                    counter1 = 31;
+                    flag = false;
+                    return new Point(544, 253);
+
+
+                case 15:
+                    counter1 = 26;
+                    flag = true;
+                    return new Point(403, 288);
+
+
+
+                case 21:
+                    counter1 = 42;
+                    flag = true;
+                    return new Point(260, 219);
+
+
+
+                case 28:
+                    counter1 = 84;
+                    flag = true;
+                    return new Point(331, 79);
+
+
+
+                case 36:
+                    counter1 = 44;
+                    flag = true;
+                    return new Point(332, 218);
+
+
+                case 51:
+                    counter1 = 67;
+                    flag = true;
+                    return new Point(437, 148);
+
+
+
+
+                case 71:
+                    counter1 = 91;
+                    flag = false;
+                    return new Point(543, 44);
+
+
+                case 78:
+                    counter1 = 98;
+                    flag = false;
+                    return new Point(300, 44);
+
+
+                case 87:
+                    counter1 = 94;
+                    flag = false;
+                    return new Point(438, 44);
+
+
+
+                // End of Ladder Switch Cases
+                // Snakes Switch for player 1
+
+
+                case 16:
+                    counter1 = 6;
+                    flag = true;
+                    return new Point(403, 364);
+
+
+                case 46:
+                    counter1 = 25;
+                    flag = true;
+                    return new Point(367, 288);
+
+
+                case 49:
+                    counter1 = 11;
+                    flag = false;
+                    return new Point(544, 325);
+
+
+                case 62:
+                    counter1 = 19;
+                    flag = false;
+                    return new Point(261, 323);
+
+
+                case 64:
+                    counter1 = 60;
+                    flag = false;
+                    return new Point(226, 182);
+
+
+                case 74:
+                    counter1 = 53;
+                    flag = false;
+                    return new Point(473, 182);
+
+
+                case 92:
+                    counter1 = 88;
+                    flag = true;
+                    return new Point(472, 80);
+
+
+                case 95:
+                    counter1 = 75;
+                    flag = false;
+                    return new Point(402, 113);
+
+
+                case 99:
+                    counter1 = 80;
+                    flag = false;
+                    return new Point(226, 113);
+
+                // End of Snakes Switch Cases
+
+
+                default:
+                    return p;
+
+
+            }
+
+
+        }
+
+
+        // Switch Method for player 2
+
+        public static Point SwitchPoint2(int counter, Point p)
+        {
+
+            // Ladder Switch for Player 2
+            switch (counter)
+            {
+                case 2:
+                    counter2 = 38;
+                    flag7 = false;
+                    return new Point(298, 254);
+
+
+
+
+                case 7:
+                    counter2 = 14;
+                    flag7 = false;
+                    return new Point(439, 323);
+
+
+                case 8:
+                    counter2 = 31;
+                    flag7 = false;
+                    return new Point(544, 253);
+
+
+                case 15:
+                    counter2 = 26;
+                    flag7 = true;
+                    return new Point(403, 288);
+
+
+
+                case 21:
+                    counter2 = 42;
+                    flag7 = true;
+                    return new Point(260, 219);
+
+
+
+                case 28:
+                    counter2 = 84;
+                    flag7 = true;
+                    return new Point(331, 79);
+
+
+
+                case 36:
+                    counter2 = 44;
+                    flag7 = true;
+                    return new Point(332, 218);
+
+
+                case 51:
+                    counter2 = 67;
+                    flag7 = true;
+                    return new Point(437, 148);
+
+
+
+
+                case 71:
+                    counter2 = 91;
+                    flag7 = false;
+                    return new Point(543, 44);
+
+
+                case 78:
+                    counter2 = 98;
+                    flag7 = false;
+                    return new Point(300, 44);
+
+
+                case 87:
+                    counter2 = 94;
+                    flag7 = false;
+                    return new Point(438, 44);
+
+
+
+                // End of Ladder Switch Cases
+                // Snakes Switch for player 1
+
+
+                case 16:
+                    counter2 = 6;
+                    flag7 = true;
+                    return new Point(403, 364);
+
+
+                case 46:
+                    counter1 = 25;
+                    flag = true;
+                    return new Point(367, 288);
+
+
+                case 49:
+                    counter2 = 11;
+                    flag7 = false;
+                    return new Point(544, 325);
+
+
+                case 62:
+                    counter2 = 19;
+                    flag7 = false;
+                    return new Point(261, 323);
+
+
+                case 64:
+                    counter2 = 60;
+                    flag7 = false;
+                    return new Point(226, 182);
+
+
+                case 74:
+                    counter2 = 53;
+                    flag7 = false;
+                    return new Point(473, 182);
+
+
+                case 92:
+                    counter2 = 88;
+                    flag7 = true;
+                    return new Point(472, 80);
+
+
+                case 95:
+                    counter2 = 75;
+                    flag7 = false;
+                    return new Point(402, 113);
+
+
+                case 99:
+                    counter2 = 80;
+                    flag7 = false;
+                    return new Point(226, 113);
+
+                // End of Snakes Switch Cases
+
+
+                default:
+                    return p;
+
+
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snakes_and_Ladders
+{
+    public partial class Form1 : Form
+    {
+      
+        private bool flag2 = true; // Display Player  No. and which player will make move 
+       
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int DiceNumber = rnd.Next(1, 7);
+            textBox2.Text = DiceNumber.ToString();
+
+            if (flag2 == true)
+            {
+                textBox1.Text = "1";
+                flag2 = false;
+            }
+            else
+            {
+                textBox1.Text = "2";
+                flag2 = true;
+            }
+
+
+            int x1 = this.pictureBox2.Location.X;
+            int y1 = this.pictureBox2.Location.Y;
+            int x2 = this.pictureBox3.Location.X;
+            int y2 = this.pictureBox3.Location.Y;
+
+
+
+            for (int i = 0; i < DiceNumber; i++)
+            {
+                if (Switch.counter1 == 100) // Fourth if Display the Winner and Close hte form
+                {
+
+                    this.pictureBox2.Location = new Point(x1 = 226, y1 = 44);
+                    MessageBox.Show($"The Winner Is Player 1");
+                    this.Close();
+
+                }
+                else if (Switch.counter2 == 100)
+                {
+                    this.pictureBox3.Location = new Point(x2 = 226, y2 = 44);
+
+                    MessageBox.Show($"The Winner Is Player 2");
+                    this.Close();
+                }
+                else
+                {
+                    if (flag2 == false) // First If  Control Which Player will Play
+                    {
+                        if (Switch.flag == true) // Second if Control  direction of play
+                        {
+
+                            x1 += 35;
+                            if (x1 > 572) // Third if control transition  from row to row 
+                            {
+                                x1 = 544;
+                                y1 -= 36;
+                                Switch.flag = false;
+                            }
+                        }
+                        else if (Switch.flag == false)
+                        {
+                            x1 -= 35;
+                            if (x1 < 220)
+                            {
+
+
+                                x1 = 226;
+                                y1 -= 36;
+                               Switch.flag = true;
+
+                            }
+                        }
+
+                    }
+                    else if (flag2 == true) // Flag2 = True
+                    {
+                        if (Switch.flag7 == false)
+                        {
+                            x2 -= 35;
+                            if (x2 < 220)
+                            {
+                                x2 = 226;
+                                y2 -= 36;
+                                Switch.flag7 = true;
+                            }
+                        }
+                        else if (Switch.flag7 == true)
+                        {
+                            x2 += 35;
+                            if (x2 > 572)
+                            {
+                                x2 = 544;
+                                y2 -= 36;
+                                Switch.flag7 = false;
+                            }
+                        }
+                    }
+                }
+
+                if (textBox1.Text == "1")
+                {
+                    Switch.counter1++;
+                }else if (textBox1.Text == "2")
+                {
+                    Switch.counter2++;
+                }
+
+            }
+            if (textBox1.Text == "1")
+            {
+                Point p1 = new Point(x1, y1);
+                this.pictureBox2.Location = Switch.SwitchPoint1(Switch.counter1, p1);
+                this.textBox3.Text = (Switch.counter1).ToString();
+
+            }
+            else if (textBox1.Text == "2")
+            {
+                Point p2 = new Point(x2, y2);
+                this.pictureBox3.Location = Switch.SwitchPoint2(Switch.counter2, p2);
+                this.textBox4.Text = (Switch.counter2).ToString();
+
+
+            }
+
+        }
+    }
+}
+
+
+
+
